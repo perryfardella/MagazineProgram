93136a63ca1aec7ea632c5b96334eb65e7c6aa68
Perry Fardella <perry.fardella@live.com>
2021-02-20 22:51:19 +0800

    Add Supplement constructor

--- src/magazineprogram/Supplement.java
+++ src/magazineprogram/Supplement.java
@@ -7,4 +7,9 @@ package magazineprogram;
 public class Supplement {
     protected String name;
     protected double weeklyCost;
+    
+    Supplement(String name, double weeklyCost) {
+        this.name = name;
+        this.weeklyCost = weeklyCost;
+    }
 }
